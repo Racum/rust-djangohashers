857a450a6e5346fe5bb29383d5e7cd2226a10974
Racum <ronaldo@racum.com>
2015-12-31 20:40:47 -0200

    Removed redundant test modules in tests folder.

--- tests/django.rs
+++ tests/django.rs
@@ -1,195 +1,189 @@
+//! This is an almost line-by-line translation from the hashers' test from Django 1.9:
+//! https://github.com/django/django/blob/e403f22/tests/auth_tests/test_hashers.py
+//! ...except for some cases that don't make sense in Rust, or in the scope of this library.
+
 extern crate djangohashers;
 
-// This is an almost line-by-line translation from the hashers' test from Django 1.9:
-// https://github.com/django/django/blob/e403f22/tests/auth_tests/test_hashers.py
-// ...except for some cases that don't make sense in Rust, or in the scope of this library.
+use djangohashers::*;
 
-#[cfg(test)]
-mod tests {
-
-    use djangohashers::*;
-
-    #[test]
-    fn test_simple() {
-        let encoded = make_password("lètmein");
-        assert!(encoded.starts_with("pbkdf2_sha256$"));
-        assert!(is_password_usable(&encoded));
-        assert!(check_password("lètmein", &encoded).unwrap());
-        assert!(!check_password("lètmeinz", &encoded).unwrap());
-        // Blank passwords
-        let blank_encoded = make_password("");
-        assert!(blank_encoded.starts_with("pbkdf2_sha256$"));
-        assert!(is_password_usable(&blank_encoded));
-        assert!(check_password("", &blank_encoded).unwrap());
-        assert!(!check_password(" ", &blank_encoded).unwrap());
-    }
-
-    #[test]
-    fn test_pbkdf2() {
-        let encoded = make_password_with_settings("lètmein", "seasalt", Algorithm::PBKDF2);
-        assert!(encoded ==
-                "pbkdf2_sha256$24000$seasalt$V9DfCAVoweeLwxC/L2mb+7swhzF0XYdyQMqmusZqiTc="
-                    .to_string());
-        assert!(is_password_usable(&encoded));
-        assert!(check_password("lètmein", &encoded).unwrap());
-        assert!(!check_password("lètmeinz", &encoded).unwrap());
-        // Blank passwords
-        let blank_encoded = make_password_with_settings("", "seasalt", Algorithm::PBKDF2);
-        assert!(blank_encoded.starts_with("pbkdf2_sha256$"));
-        assert!(is_password_usable(&blank_encoded));
-        assert!(check_password("", &blank_encoded).unwrap());
-        assert!(!check_password(" ", &blank_encoded).unwrap());
-    }
-
-    #[test]
-    fn test_sha1() {
-        let encoded = make_password_with_settings("lètmein", "seasalt", Algorithm::SHA1);
-        assert!(encoded == "sha1$seasalt$cff36ea83f5706ce9aa7454e63e431fc726b2dc8".to_string());
-        assert!(is_password_usable(&encoded));
-        assert!(check_password("lètmein", &encoded).unwrap());
-        assert!(!check_password("lètmeinz", &encoded).unwrap());
-        // Blank passwords
-        let blank_encoded = make_password_with_settings("", "seasalt", Algorithm::SHA1);
-        assert!(blank_encoded.starts_with("sha1$"));
-        assert!(is_password_usable(&blank_encoded));
-        assert!(check_password("", &blank_encoded).unwrap());
-        assert!(!check_password(" ", &blank_encoded).unwrap());
-    }
-
-    #[test]
-    fn test_md5() {
-        let encoded = make_password_with_settings("lètmein", "seasalt", Algorithm::MD5);
-        assert!(encoded == "md5$seasalt$3f86d0d3d465b7b458c231bf3555c0e3".to_string());
-        assert!(is_password_usable(&encoded));
-        assert!(check_password("lètmein", &encoded).unwrap());
-        assert!(!check_password("lètmeinz", &encoded).unwrap());
-        // Blank passwords
-        let blank_encoded = make_password_with_settings("", "seasalt", Algorithm::MD5);
-        assert!(blank_encoded.starts_with("md5$"));
-        assert!(is_password_usable(&blank_encoded));
-        assert!(check_password("", &blank_encoded).unwrap());
-        assert!(!check_password(" ", &blank_encoded).unwrap());
-    }
-
-    #[test]
-    fn test_unsalted_md5() {
-        let encoded = make_password_with_settings("lètmein", "", Algorithm::UnsaltedMD5);
-        assert!(encoded == "88a434c88cca4e900f7874cd98123f43".to_string());
-        assert!(is_password_usable(&encoded));
-        assert!(check_password("lètmein", &encoded).unwrap());
-        assert!(!check_password("lètmeinz", &encoded).unwrap());
-        // Blank passwords
-        let blank_encoded = make_password_with_settings("", "", Algorithm::UnsaltedMD5);
-        assert!(check_password("", &blank_encoded).unwrap());
-        assert!(!check_password(" ", &blank_encoded).unwrap());
-    }
-
-    #[test]
-    fn test_unsalted_sha1() {
-        let encoded = make_password_with_settings("lètmein", "", Algorithm::UnsaltedSHA1);
-        assert!(encoded == "sha1$$6d138ca3ae545631b3abd71a4f076ce759c5700b".to_string());
-        assert!(is_password_usable(&encoded));
-        assert!(check_password("lètmein", &encoded).unwrap());
-        assert!(!check_password("lètmeinz", &encoded).unwrap());
-        // Raw SHA1 isn't acceptable
-        assert!(check_password("lètmein", "6d138ca3ae545631b3abd71a4f076ce759c5700b").is_err());
-        // Blank passwords
-        let blank_encoded = make_password_with_settings("", "", Algorithm::UnsaltedSHA1);
-        assert!(blank_encoded.starts_with("sha1$"));
-        assert!(is_password_usable(&blank_encoded));
-        assert!(check_password("", &blank_encoded).unwrap());
-        assert!(!check_password(" ", &blank_encoded).unwrap());
-    }
-
-    // Hasher based on UNIX's crypt(3) not implemented:
-    // - test_crypt
-
-    #[test]
-    fn test_bcrypt_sha256() {
-        let encoded = make_password_with_settings("lètmein", "", Algorithm::BCryptSHA256);
-        assert!(is_password_usable(&encoded));
-        assert!(encoded.starts_with("bcrypt_sha256$"));
-        assert!(check_password("lètmein", &encoded).unwrap());
-        assert!(!check_password("lètmeinz", &encoded).unwrap());
-        // Verify that password truncation no longer works
-        let password = "VSK0UYV6FFQVZ0KG88DYN9WADAADZO1CTSIVDJUNZSUML6IBX7LN7ZS3R5JGB3RGZ7VI7G7DJQ\
-                        9NI8BQFSRPTG6UWTTVESA5ZPUN";
-        let trunc_encoded = make_password_with_settings(password, "", Algorithm::BCryptSHA256);
-        assert!(check_password(password, &trunc_encoded).unwrap());
-        assert!(!check_password(&password[0..72], &trunc_encoded).unwrap());
-        // Blank passwords
-        let blank_encoded = make_password_with_settings("", "", Algorithm::BCryptSHA256);
-        assert!(is_password_usable(&blank_encoded));
-        assert!(blank_encoded.starts_with("bcrypt_sha256$"));
-        assert!(check_password("", &blank_encoded).unwrap());
-        assert!(!check_password(" ", &blank_encoded).unwrap());
-    }
-
-    #[test]
-    fn test_bcrypt() {
-        let encoded = make_password_with_settings("lètmein", "", Algorithm::BCrypt);
-        assert!(is_password_usable(&encoded));
-        assert!(encoded.starts_with("bcrypt$"));
-        assert!(check_password("lètmein", &encoded).unwrap());
-        assert!(!check_password("lètmeinz", &encoded).unwrap());
-        // Blank passwords
-        let blank_encoded = make_password_with_settings("", "", Algorithm::BCrypt);
-        assert!(is_password_usable(&blank_encoded));
-        assert!(blank_encoded.starts_with("bcrypt$"));
-        assert!(check_password("", &blank_encoded).unwrap());
-        assert!(!check_password(" ", &blank_encoded).unwrap());
-    }
-
-    // This library does not fire upgrade callbacks:
-    // - test_bcrypt_upgrade
-
-    #[test]
-    fn test_unusable() {
-        let encoded = "!Q24gQu9Sy3X1PJPCaEMTRrw5eLFWY8htI2FsqCbC"; // From make_password(None)
-        assert!(encoded.len() == 41);
-        assert!(!is_password_usable(&encoded));
-        assert!(check_password(&encoded, &encoded).is_err());
-        assert!(check_password("!", &encoded).is_err());
-        assert!(check_password("", &encoded).is_err());
-        assert!(check_password("lètmein", &encoded).is_err());
-        assert!(check_password("lètmeinz", &encoded).is_err());
-    }
-
-    // Scenario not possible during run time:
-    // - test_unspecified_password
-    // - test_bad_algorithm
-
-    #[test]
-    fn test_bad_encoded() {
-        assert!(!is_password_usable("lètmein_badencoded"));
-        assert!(!is_password_usable(""));
-    }
-
-    #[test]
-    fn test_low_level_pbkdf2() {
-        let encoded = make_password_with_settings("lètmein", "seasalt2", Algorithm::PBKDF2);
-        assert!(encoded ==
-                "pbkdf2_sha256$24000$seasalt2$TUDkfilKHVC7BkaKSZgIKhm0aTtXlmcw/5C1FeS/DPk="
-                    .to_string());
-        assert!(check_password("lètmein", &encoded).unwrap());
-    }
-
-    #[test]
-    fn test_low_level_pbkdf2_sha1() {
-        let encoded = make_password_with_settings("lètmein", "seasalt2", Algorithm::PBKDF2SHA1);
-        assert!(encoded == "pbkdf2_sha1$24000$seasalt2$L37ETdd9trqrsJDwapU3P+2Edhg=".to_string());
-        assert!(check_password("lètmein", &encoded).unwrap());
-    }
-
-    // This library does not fire upgrade callbacks:
-    // - test_upgrade
-    // - test_no_upgrade
-    // - test_no_upgrade_on_incorrect_pass
-    // - test_pbkdf2_upgrade
-    // - test_pbkdf2_upgrade_new_hasher
-
-    // Scenario not possible during run time:
-    // - test_load_library_no_algorithm
-    // - test_load_library_importerror
-
+#[test]
+fn test_simple() {
+    let encoded = make_password("lètmein");
+    assert!(encoded.starts_with("pbkdf2_sha256$"));
+    assert!(is_password_usable(&encoded));
+    assert!(check_password("lètmein", &encoded).unwrap());
+    assert!(!check_password("lètmeinz", &encoded).unwrap());
+    // Blank passwords
+    let blank_encoded = make_password("");
+    assert!(blank_encoded.starts_with("pbkdf2_sha256$"));
+    assert!(is_password_usable(&blank_encoded));
+    assert!(check_password("", &blank_encoded).unwrap());
+    assert!(!check_password(" ", &blank_encoded).unwrap());
 }
+
+#[test]
+fn test_pbkdf2() {
+    let encoded = make_password_with_settings("lètmein", "seasalt", Algorithm::PBKDF2);
+    assert!(encoded ==
+            "pbkdf2_sha256$24000$seasalt$V9DfCAVoweeLwxC/L2mb+7swhzF0XYdyQMqmusZqiTc=".to_string());
+    assert!(is_password_usable(&encoded));
+    assert!(check_password("lètmein", &encoded).unwrap());
+    assert!(!check_password("lètmeinz", &encoded).unwrap());
+    // Blank passwords
+    let blank_encoded = make_password_with_settings("", "seasalt", Algorithm::PBKDF2);
+    assert!(blank_encoded.starts_with("pbkdf2_sha256$"));
+    assert!(is_password_usable(&blank_encoded));
+    assert!(check_password("", &blank_encoded).unwrap());
+    assert!(!check_password(" ", &blank_encoded).unwrap());
+}
+
+#[test]
+fn test_sha1() {
+    let encoded = make_password_with_settings("lètmein", "seasalt", Algorithm::SHA1);
+    assert!(encoded == "sha1$seasalt$cff36ea83f5706ce9aa7454e63e431fc726b2dc8".to_string());
+    assert!(is_password_usable(&encoded));
+    assert!(check_password("lètmein", &encoded).unwrap());
+    assert!(!check_password("lètmeinz", &encoded).unwrap());
+    // Blank passwords
+    let blank_encoded = make_password_with_settings("", "seasalt", Algorithm::SHA1);
+    assert!(blank_encoded.starts_with("sha1$"));
+    assert!(is_password_usable(&blank_encoded));
+    assert!(check_password("", &blank_encoded).unwrap());
+    assert!(!check_password(" ", &blank_encoded).unwrap());
+}
+
+#[test]
+fn test_md5() {
+    let encoded = make_password_with_settings("lètmein", "seasalt", Algorithm::MD5);
+    assert!(encoded == "md5$seasalt$3f86d0d3d465b7b458c231bf3555c0e3".to_string());
+    assert!(is_password_usable(&encoded));
+    assert!(check_password("lètmein", &encoded).unwrap());
+    assert!(!check_password("lètmeinz", &encoded).unwrap());
+    // Blank passwords
+    let blank_encoded = make_password_with_settings("", "seasalt", Algorithm::MD5);
+    assert!(blank_encoded.starts_with("md5$"));
+    assert!(is_password_usable(&blank_encoded));
+    assert!(check_password("", &blank_encoded).unwrap());
+    assert!(!check_password(" ", &blank_encoded).unwrap());
+}
+
+#[test]
+fn test_unsalted_md5() {
+    let encoded = make_password_with_settings("lètmein", "", Algorithm::UnsaltedMD5);
+    assert!(encoded == "88a434c88cca4e900f7874cd98123f43".to_string());
+    assert!(is_password_usable(&encoded));
+    assert!(check_password("lètmein", &encoded).unwrap());
+    assert!(!check_password("lètmeinz", &encoded).unwrap());
+    // Blank passwords
+    let blank_encoded = make_password_with_settings("", "", Algorithm::UnsaltedMD5);
+    assert!(check_password("", &blank_encoded).unwrap());
+    assert!(!check_password(" ", &blank_encoded).unwrap());
+}
+
+#[test]
+fn test_unsalted_sha1() {
+    let encoded = make_password_with_settings("lètmein", "", Algorithm::UnsaltedSHA1);
+    assert!(encoded == "sha1$$6d138ca3ae545631b3abd71a4f076ce759c5700b".to_string());
+    assert!(is_password_usable(&encoded));
+    assert!(check_password("lètmein", &encoded).unwrap());
+    assert!(!check_password("lètmeinz", &encoded).unwrap());
+    // Raw SHA1 isn't acceptable
+    assert!(check_password("lètmein", "6d138ca3ae545631b3abd71a4f076ce759c5700b").is_err());
+    // Blank passwords
+    let blank_encoded = make_password_with_settings("", "", Algorithm::UnsaltedSHA1);
+    assert!(blank_encoded.starts_with("sha1$"));
+    assert!(is_password_usable(&blank_encoded));
+    assert!(check_password("", &blank_encoded).unwrap());
+    assert!(!check_password(" ", &blank_encoded).unwrap());
+}
+
+// Hasher based on UNIX's crypt(3) not implemented:
+// - test_crypt
+
+#[test]
+fn test_bcrypt_sha256() {
+    let encoded = make_password_with_settings("lètmein", "", Algorithm::BCryptSHA256);
+    assert!(is_password_usable(&encoded));
+    assert!(encoded.starts_with("bcrypt_sha256$"));
+    assert!(check_password("lètmein", &encoded).unwrap());
+    assert!(!check_password("lètmeinz", &encoded).unwrap());
+    // Verify that password truncation no longer works
+    let password = "VSK0UYV6FFQVZ0KG88DYN9WADAADZO1CTSIVDJUNZSUML6IBX7LN7ZS3R5JGB3RGZ7VI7G7DJQ9NI8\
+                    BQFSRPTG6UWTTVESA5ZPUN";
+    let trunc_encoded = make_password_with_settings(password, "", Algorithm::BCryptSHA256);
+    assert!(check_password(password, &trunc_encoded).unwrap());
+    assert!(!check_password(&password[0..72], &trunc_encoded).unwrap());
+    // Blank passwords
+    let blank_encoded = make_password_with_settings("", "", Algorithm::BCryptSHA256);
+    assert!(is_password_usable(&blank_encoded));
+    assert!(blank_encoded.starts_with("bcrypt_sha256$"));
+    assert!(check_password("", &blank_encoded).unwrap());
+    assert!(!check_password(" ", &blank_encoded).unwrap());
+}
+
+#[test]
+fn test_bcrypt() {
+    let encoded = make_password_with_settings("lètmein", "", Algorithm::BCrypt);
+    assert!(is_password_usable(&encoded));
+    assert!(encoded.starts_with("bcrypt$"));
+    assert!(check_password("lètmein", &encoded).unwrap());
+    assert!(!check_password("lètmeinz", &encoded).unwrap());
+    // Blank passwords
+    let blank_encoded = make_password_with_settings("", "", Algorithm::BCrypt);
+    assert!(is_password_usable(&blank_encoded));
+    assert!(blank_encoded.starts_with("bcrypt$"));
+    assert!(check_password("", &blank_encoded).unwrap());
+    assert!(!check_password(" ", &blank_encoded).unwrap());
+}
+
+// This library does not fire upgrade callbacks:
+// - test_bcrypt_upgrade
+
+#[test]
+fn test_unusable() {
+    let encoded = "!Q24gQu9Sy3X1PJPCaEMTRrw5eLFWY8htI2FsqCbC"; // From make_password(None)
+    assert!(encoded.len() == 41);
+    assert!(!is_password_usable(&encoded));
+    assert!(check_password(&encoded, &encoded).is_err());
+    assert!(check_password("!", &encoded).is_err());
+    assert!(check_password("", &encoded).is_err());
+    assert!(check_password("lètmein", &encoded).is_err());
+    assert!(check_password("lètmeinz", &encoded).is_err());
+}
+
+// Scenario not possible during run time:
+// - test_unspecified_password
+// - test_bad_algorithm
+
+#[test]
+fn test_bad_encoded() {
+    assert!(!is_password_usable("lètmein_badencoded"));
+    assert!(!is_password_usable(""));
+}
+
+#[test]
+fn test_low_level_pbkdf2() {
+    let encoded = make_password_with_settings("lètmein", "seasalt2", Algorithm::PBKDF2);
+    assert!(encoded ==
+            "pbkdf2_sha256$24000$seasalt2$TUDkfilKHVC7BkaKSZgIKhm0aTtXlmcw/5C1FeS/DPk="
+                .to_string());
+    assert!(check_password("lètmein", &encoded).unwrap());
+}
+
+#[test]
+fn test_low_level_pbkdf2_sha1() {
+    let encoded = make_password_with_settings("lètmein", "seasalt2", Algorithm::PBKDF2SHA1);
+    assert!(encoded == "pbkdf2_sha1$24000$seasalt2$L37ETdd9trqrsJDwapU3P+2Edhg=".to_string());
+    assert!(check_password("lètmein", &encoded).unwrap());
+}
+
+// This library does not fire upgrade callbacks:
+// - test_upgrade
+// - test_no_upgrade
+// - test_no_upgrade_on_incorrect_pass
+// - test_pbkdf2_upgrade
+// - test_pbkdf2_upgrade_new_hasher
+
+// Scenario not possible during run time:
+// - test_load_library_no_algorithm
+// - test_load_library_importerror
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,111 +1,103 @@
-#[macro_use]
 extern crate djangohashers;
 
+use djangohashers::*;
 
-#[cfg(test)]
-mod tests {
+static PASSWORD: &'static str = "ExjGmyUT73bFoT";
+static SALT: &'static str = "KQ8zeK6wKRuR";
 
-    use djangohashers::*;
+#[test]
+fn test_pbkdf2_sha256() {
+    let encoded = make_password_with_settings(PASSWORD, SALT, Algorithm::PBKDF2);
+    let h = "pbkdf2_sha256$24000$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
+    assert!(encoded == h.to_string());
+    assert!(check_password(PASSWORD, &encoded).unwrap());
+}
 
-    static PASSWORD: &'static str = "ExjGmyUT73bFoT";
-    static SALT: &'static str = "KQ8zeK6wKRuR";
+#[test]
+fn test_pbkdf2_sha1() {
+    let encoded = make_password_with_settings(PASSWORD, SALT, Algorithm::PBKDF2SHA1);
+    let h = "pbkdf2_sha1$24000$KQ8zeK6wKRuR$tSJh4xdxfMJotlxfkCGjTFpGYZU=";
+    assert!(encoded == h.to_string());
+    assert!(check_password(PASSWORD, &encoded).unwrap());
+}
 
-    #[test]
-    fn test_pbkdf2_sha256() {
-        let encoded = make_password_with_settings(PASSWORD, SALT, Algorithm::PBKDF2);
-        let h = "pbkdf2_sha256$24000$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
-        assert!(encoded == h.to_string());
-        assert!(check_password(PASSWORD, &encoded).unwrap());
-    }
+#[test]
+fn test_sha1() {
+    let encoded = make_password_with_settings(PASSWORD, SALT, Algorithm::SHA1);
+    let h = "sha1$KQ8zeK6wKRuR$f83371bca01fa6089456e673ccfb17f42d810b00";
+    assert!(encoded == h.to_string());
+    assert!(check_password(PASSWORD, &encoded).unwrap());
+}
 
-    #[test]
-    fn test_pbkdf2_sha1() {
-        let encoded = make_password_with_settings(PASSWORD, SALT, Algorithm::PBKDF2SHA1);
-        let h = "pbkdf2_sha1$24000$KQ8zeK6wKRuR$tSJh4xdxfMJotlxfkCGjTFpGYZU=";
-        assert!(encoded == h.to_string());
-        assert!(check_password(PASSWORD, &encoded).unwrap());
-    }
+#[test]
+fn test_md5() {
+    let encoded = make_password_with_settings(PASSWORD, SALT, Algorithm::MD5);
+    let h = "md5$KQ8zeK6wKRuR$0137e4d74cb2d9ed9cb1a5f391f6175e";
+    assert!(encoded == h.to_string());
+    assert!(check_password(PASSWORD, &encoded).unwrap());
+}
 
-    #[test]
-    fn test_sha1() {
-        let encoded = make_password_with_settings(PASSWORD, SALT, Algorithm::SHA1);
-        let h = "sha1$KQ8zeK6wKRuR$f83371bca01fa6089456e673ccfb17f42d810b00";
-        assert!(encoded == h.to_string());
-        assert!(check_password(PASSWORD, &encoded).unwrap());
-    }
+#[test]
+fn test_unsalted_md5() {
+    let encoded = make_password_with_settings(PASSWORD, "", Algorithm::UnsaltedMD5);
+    let h = "7cf6409a82cd4c8b96a9ecf6ad679119";
+    assert!(encoded == h.to_string());
+    assert!(check_password(PASSWORD, &encoded).unwrap());
+}
 
-    #[test]
-    fn test_md5() {
-        let encoded = make_password_with_settings(PASSWORD, SALT, Algorithm::MD5);
-        let h = "md5$KQ8zeK6wKRuR$0137e4d74cb2d9ed9cb1a5f391f6175e";
-        assert!(encoded == h.to_string());
-        assert!(check_password(PASSWORD, &encoded).unwrap());
-    }
+#[test]
+fn test_unsalted_sha1() {
+    let encoded = make_password_with_settings(PASSWORD, "", Algorithm::UnsaltedSHA1);
+    let h = "sha1$$22e6217f026c7a395f0840c1ffbdb163072419e7";
+    assert!(encoded == h.to_string());
+    assert!(check_password(PASSWORD, &encoded).unwrap());
+}
 
-    #[test]
-    fn test_unsalted_md5() {
-        let encoded = make_password_with_settings(PASSWORD, "", Algorithm::UnsaltedMD5);
-        let h = "7cf6409a82cd4c8b96a9ecf6ad679119";
-        assert!(encoded == h.to_string());
-        assert!(check_password(PASSWORD, &encoded).unwrap());
-    }
+#[test]
+fn test_bcrypt_sha256() {
+    let encoded = make_password_with_settings(PASSWORD, "", Algorithm::BCryptSHA256);
+    assert!(check_password(PASSWORD, &encoded).unwrap());
+    let h = "bcrypt_sha256$$2b$12$LZSJchsWG/DrBy1erNs4eeYo6tZNlLFQmONdxN9HPesa1EyXVcTXK";
+    assert!(check_password(PASSWORD, h).unwrap());
+}
 
-    #[test]
-    fn test_unsalted_sha1() {
-        let encoded = make_password_with_settings(PASSWORD, "", Algorithm::UnsaltedSHA1);
-        let h = "sha1$$22e6217f026c7a395f0840c1ffbdb163072419e7";
-        assert!(encoded == h.to_string());
-        assert!(check_password(PASSWORD, &encoded).unwrap());
-    }
+#[test]
+fn test_bcrypt() {
+    let encoded = make_password_with_settings(PASSWORD, "", Algorithm::BCrypt);
+    assert!(check_password(PASSWORD, &encoded).unwrap());
+    let h = "bcrypt$$2b$12$LZSJchsWG/DrBy1erNs4ee31eJ7DaWiuwhDOC7aqIyqGGggfu6Y/.";
+    assert!(check_password(PASSWORD, h).unwrap());
+}
 
-    #[test]
-    fn test_bcrypt_sha256() {
-        let encoded = make_password_with_settings(PASSWORD, "", Algorithm::BCryptSHA256);
-        assert!(check_password(PASSWORD, &encoded).unwrap());
-        let h = "bcrypt_sha256$$2b$12$LZSJchsWG/DrBy1erNs4eeYo6tZNlLFQmONdxN9HPesa1EyXVcTXK";
-        assert!(check_password(PASSWORD, h).unwrap());
-    }
+#[test]
+fn test_is_password_usable() {
+    // Good hashes:
+    assert!(is_password_usable("pbkdf2_sha1$24000$KQ8zeK6wKRuR$tSJh4xdxfMJotlxfkCGjTFpGYZU="));
+    assert!(is_password_usable("7cf6409a82cd4c8b96a9ecf6ad679119"));
+    // Bad hashes:
+    assert!(!is_password_usable(""));
+    assert!(!is_password_usable("password"));
+    assert!(!is_password_usable("!cf6409a82cd4c8b96a9ecf6ad679119"));
+}
 
-    #[test]
-    fn test_bcrypt() {
-        let encoded = make_password_with_settings(PASSWORD, "", Algorithm::BCrypt);
-        assert!(check_password(PASSWORD, &encoded).unwrap());
-        let h = "bcrypt$$2b$12$LZSJchsWG/DrBy1erNs4ee31eJ7DaWiuwhDOC7aqIyqGGggfu6Y/.";
-        assert!(check_password(PASSWORD, h).unwrap());
-    }
+#[test]
+fn test_check_password_tolerant() {
+    let negative = "pbkdf2_sha256$-24000$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
+    assert!(!check_password_tolerant(PASSWORD, negative));
+    let nan = "pbkdf2_sha256$NaN$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
+    assert!(!check_password_tolerant(PASSWORD, nan));
+    let rot13 = "rot13$1$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
+    assert!(!check_password_tolerant(PASSWORD, rot13));
+    assert!(!check_password_tolerant(PASSWORD, ""));
+}
 
-    #[test]
-    fn test_is_password_usable() {
-        // Good hashes:
-        assert!(is_password_usable("pbkdf2_sha1$24000$KQ8zeK6wKRuR$tSJh4xdxfMJotlxfkCGjTFpGYZU="));
-        assert!(is_password_usable("7cf6409a82cd4c8b96a9ecf6ad679119"));
-        // Bad hashes:
-        assert!(!is_password_usable(""));
-        assert!(!is_password_usable("password"));
-        assert!(!is_password_usable("!cf6409a82cd4c8b96a9ecf6ad679119"));
-    }
-
-    #[test]
-    fn test_check_password_tolerant() {
-        let negative = "pbkdf2_sha256$-24000$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
-        assert!(!check_password_tolerant(PASSWORD, negative));
-        let nan = "pbkdf2_sha256$NaN$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
-        assert!(!check_password_tolerant(PASSWORD, nan));
-        let rot13 = "rot13$1$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
-        assert!(!check_password_tolerant(PASSWORD, rot13));
-        assert!(!check_password_tolerant(PASSWORD, ""));
-    }
-
-    #[test]
-    fn test_errors() {
-        let negative = "pbkdf2_sha256$-24000$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
-        assert!(check_password(PASSWORD, negative) == Err(HasherError::InvalidIterations));
-        let nan = "pbkdf2_sha256$NaN$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
-        assert!(check_password(PASSWORD, nan) == Err(HasherError::InvalidIterations));
-        let rot13 = "rot13$1$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
-        assert!(check_password(PASSWORD, rot13) == Err(HasherError::UnknownAlgorithm));
-        assert!(check_password(PASSWORD, "") == Err(HasherError::EmptyHash));
-    }
-
-
+#[test]
+fn test_errors() {
+    let negative = "pbkdf2_sha256$-24000$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
+    assert!(check_password(PASSWORD, negative) == Err(HasherError::InvalidIterations));
+    let nan = "pbkdf2_sha256$NaN$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
+    assert!(check_password(PASSWORD, nan) == Err(HasherError::InvalidIterations));
+    let rot13 = "rot13$1$KQ8zeK6wKRuR$cmhbSt1XVKuO4FGd9+AX8qSBD4Z0395nZatXTJpEtTY=";
+    assert!(check_password(PASSWORD, rot13) == Err(HasherError::UnknownAlgorithm));
+    assert!(check_password(PASSWORD, "") == Err(HasherError::EmptyHash));
 }
