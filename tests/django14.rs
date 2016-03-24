1a5d1132fcbfc0479ca76a70998fd539da6e4548
Racum <ronaldo@racum.com>
2016-03-24 19:20:03 -0300

    Added tests from Django 1.4 to 1.7.

---
+++ tests/django14.rs
@@ -0,0 +1,43 @@
+//! This is an almost line-by-line translation from the hashers' test from Django 1.4:
+//! https://github.com/django/django/blob/2591fb8/django/contrib/auth/tests/hashers.py
+//! ...but only for the tests where the iterations differ from Django 1.9.
+
+extern crate djangohashers;
+
+use djangohashers::*;
+
+
+#[test]
+fn test_pbkdf2() {
+    let django = Django {version: Version::V14};
+    let encoded = django.make_password_with_settings("letmein", "seasalt", Algorithm::PBKDF2);
+    assert!(encoded ==
+            "pbkdf2_sha256$10000$seasalt$FQCNpiZpTb0zub+HBsH6TOwyRxJ19FwvjbweatNmK/Y=".to_string());
+    assert!(is_password_usable(&encoded));
+    assert!(check_password("letmein", &encoded).unwrap());
+    assert!(!check_password("letmeinz", &encoded).unwrap());
+    // Blank passwords
+    let blank_encoded = django.make_password_with_settings("", "seasalt", Algorithm::PBKDF2);
+    assert!(blank_encoded.starts_with("pbkdf2_sha256$"));
+    assert!(is_password_usable(&blank_encoded));
+    assert!(check_password("", &blank_encoded).unwrap());
+    assert!(!check_password(" ", &blank_encoded).unwrap());
+}
+
+#[test]
+fn test_low_level_pbkdf2() {
+    let django = Django {version: Version::V14};
+    let encoded = django.make_password_with_settings("letmein", "seasalt", Algorithm::PBKDF2);
+    assert!(encoded ==
+            "pbkdf2_sha256$10000$seasalt$FQCNpiZpTb0zub+HBsH6TOwyRxJ19FwvjbweatNmK/Y="
+                .to_string());
+    assert!(check_password("letmein", &encoded).unwrap());
+}
+
+#[test]
+fn test_low_level_pbkdf2_sha1() {
+    let django = Django {version: Version::V14};
+    let encoded = django.make_password_with_settings("letmein", "seasalt", Algorithm::PBKDF2SHA1);
+    assert!(encoded == "pbkdf2_sha1$10000$seasalt$91JiNKgwADC8j2j86Ije/cc4vfQ=".to_string());
+    assert!(check_password("letmein", &encoded).unwrap());
+}
---
+++ tests/django16.rs
@@ -0,0 +1,43 @@
+//! This is an almost line-by-line translation from the hashers' test from Django 1.8:
+//! https://github.com/django/django/blob/d92b085/django/contrib/auth/tests/test_hashers.py
+//! ...but only for the tests where the iterations differ from Django 1.9.
+
+extern crate djangohashers;
+
+use djangohashers::*;
+
+
+#[test]
+fn test_pbkdf2() {
+    let django = Django {version: Version::V16};
+    let encoded = django.make_password_with_settings("lètmein", "seasalt", Algorithm::PBKDF2);
+    assert!(encoded ==
+            "pbkdf2_sha256$12000$seasalt$Ybw8zsFxqja97tY/o6G+Fy1ksY4U/Hw3DRrGED6Up4s=".to_string());
+    assert!(is_password_usable(&encoded));
+    assert!(check_password("lètmein", &encoded).unwrap());
+    assert!(!check_password("lètmeinz", &encoded).unwrap());
+    // Blank passwords
+    let blank_encoded = django.make_password_with_settings("", "seasalt", Algorithm::PBKDF2);
+    assert!(blank_encoded.starts_with("pbkdf2_sha256$"));
+    assert!(is_password_usable(&blank_encoded));
+    assert!(check_password("", &blank_encoded).unwrap());
+    assert!(!check_password(" ", &blank_encoded).unwrap());
+}
+
+#[test]
+fn test_low_level_pbkdf2() {
+    let django = Django {version: Version::V16};
+    let encoded = django.make_password_with_settings("lètmein", "seasalt2", Algorithm::PBKDF2);
+    assert!(encoded ==
+            "pbkdf2_sha256$12000$seasalt2$hlDLKsxgkgb1aeOppkM5atCYw5rPzAjCNQZ4NYyUROw="
+                .to_string());
+    assert!(check_password("lètmein", &encoded).unwrap());
+}
+
+#[test]
+fn test_low_level_pbkdf2_sha1() {
+    let django = Django {version: Version::V16};
+    let encoded = django.make_password_with_settings("lètmein", "seasalt2", Algorithm::PBKDF2SHA1);
+    assert!(encoded == "pbkdf2_sha1$12000$seasalt2$JeMRVfjjgtWw3/HzlnlfqBnQ6CA=".to_string());
+    assert!(check_password("lètmein", &encoded).unwrap());
+}
